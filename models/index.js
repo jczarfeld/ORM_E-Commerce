4ebabb2885879b13087cf92e5706be96dc4960d2
John Osterfeld <jczarfeld@gmail.com>
2021-07-29 10:02:41 -0400

    working on sequelize assosciations

--- models/index.js
+++ models/index.js
@@ -4,6 +4,29 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Tag.belongsToMany(Product, {
+  through:ProductTag,
+  foreignKey: "tag_id"
+
+});
+
+Product.belongsToMany(Tag, {
+  through:ProductTag,
+  foreignKey: "product_id"
+});
+
+
+
 // Products belongsTo Category
 
 // Categories have many Products
